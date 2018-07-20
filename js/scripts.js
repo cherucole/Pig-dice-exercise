4b69d4526bebde181162df43768541ff9e0913ad
cherucole <cheruyotcheruyot@gmail.com>
2018-07-20 18:25:35 +0300

    Fixed issues on roll button

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,103 @@
-function Player(playerName) {
-  this.playerName = playerName;
-  this.score = 0;
+function Player(userName) {
+    this.userName = userName;
+    this.score = 0;
+
+}
+
+function Turn(player) {
+    this.total = 0;
+    this.randNumber = 0;
+    this.player = player;
 };
 
-function takeTurn(player) {
-  this.sum = 0;
-  this.sumNumbers = 0;
-  this.player = player;
+Turn.prototype.diceRoller = function(player1, player2) {
+    var randNumber = Math.floor(Math.random() * 6) + 1;
+    this.total += randNumber;
 
+    if (randNumber == 1) {
+        this.total = 0;
+        this.endTurn(player1, player2);
+
+        return randNumber;
+    } else {
+        this.randNumber += randNumber;
+        return randNumber;
+    };
 };
 
-takeTurn.prototype.rollDice = function(playerOne, playerTwo) {
-  var sumNumbers = Math.floor(Math.random() * 6) + 1;
-  this.sum += sumNumbers;
-
-  if (sumNumbers == 1) {
-    this.sum = 0;
-    this.endTurn(playerOne, playerTwo);
-    return sumNumbers;
-  } else {
-    this.sumNumbers += sumNumbers;
-    return sumNumbers;
-  };
-};
-
-takeTurn.prototype.endTurn = function(playerOne, playerTwo) {
-  this.player.score += this.sum;
-  this.sum = 0;
-  this.sumNumbers = 0;
-  if (this.player == playerOne) {
-    this.player = playerTwo;
-    $("#playerTwo").toggleClass("active");
-    $("#playerOne").toggleClass("active");
-  } else if (this.player == playerTwo) {
-    this.player = playerOne;
-    $("#playerTwo").toggleClass("active");
-    $("#playerOne").toggleClass("active");
-
-  };
-
+Turn.prototype.endTurn = function(player1, player2) {
+    this.player.score += this.total;
+    this.total = 0;
+    this.randNumber = 0;
+    if (this.player == player1) {
+        this.player = player2;
+        $("#player2").toggleClass("active");
+        $("#player1").toggleClass("active");
+    } else if (this.player == player2) {
+        this.player = player1;
+        $("#player2").toggleClass("active");
+        $("#player1").toggleClass("active");
+    };
 };
 $(document).ready(function() {
-  var playerOne = new Player("Player One");
-  var playerTwo = new Player("Player Two");
+    var player1 = new Player("Player 1");
+    var player2 = new Player("Player 2");
 
-  var currentTurn = new takeTurn(playerOne);
-  var sum = currentTurn.sum;
-  $("#cumulScore").text(sum);
-  $("#playerOneScore").text(playerOne.score);
-  $("#playerTwoScore").text(playerTwo.score);
-  $("#current_player").text(currentTurn.player.playerName);
-  $("form#roll").submit(function(event) {
-    event.preventDefault();
 
-    var result = currentTurn.rollDice(playerOne, playerTwo);
+    var currentTurn = new Turn(player1);
 
-    $('#diceScore').text(result);
-    $('#cumulScore').text(currentTurn.sum);
+    var total = currentTurn.total;
 
-    if ((currentTurn.sum + currentTurn.player.score) >= 100) {
-      if (currentTurn.player == playerOne) {
-        $('#playerOneScore').text(currentTurn.sum + currentTurn.player.score);
-        alert("You are the winner!");
-      } else if (currentTurn.player == playerTwo) {
-        $('#playerTwoScore').text(currentTurn.sum + currentTurn.player.score)
-        alert("You are the winner!");
-      };
-    };
-  });
+    //Prints initial Turn Total of 0
+    $("#roll-total").text(total);
 
-  $("form#endTurn").submit(function(event) {
-    event.preventDefault();
+    //Prints initial player scores of 0
+    $('#player1-score').text(player1.score);
+    $('#player2-score').text(player2.score);
 
-    currentTurn.endTurn(playerOne, playerTwo);
+    //Prints current Player
+    $('#current_player').text(currentTurn.player.userName);
 
-    $('#current_player').text(currentTurn.player.playerName);
+    //This code runs when you click the Roll button
+    $("form#roller").submit(function(event) {
+        event.preventDefault();
 
-    $('#playerOneScore').text(playerOne.score);
-    $('#playerTwoScore').text(playerTwo.score);
+        //Creates a dice roll number
+        var result = currentTurn.diceRoller(player1, player2);
 
-    $('#roll').text(currentTurn.sumNumbers);
-    $('#cumulScore').text(currentTurn.sum);
-  });
+        //Prints the roll number to the page
+        $('#roll').text(result);
+
+        //Prints the roll total to the page
+        $('#roll-total').text(currentTurn.total);
+
+        //Determines the winner and prints player score to page
+        if ((currentTurn.total + currentTurn.player.score) >= 100) {
+            if (currentTurn.player == player1) {
+                $('#player1-score').text(currentTurn.total + currentTurn.player.score);
+                alert("You are the winner!");
+            } else if (currentTurn.player == player2) {
+                $('#player2-score').text(currentTurn.total + currentTurn.player.score)
+                alert("You are the winner!");
+            };
+        };
+    });
+
+    //This code runs when you click the End Turn button
+    $("form#end-turn").submit(function(event) {
+        event.preventDefault();
+
+        currentTurn.endTurn(player1, player2);
+
+        //Prints current Player
+        $('#current_player').text(currentTurn.player.userName);
+
+        //Prints players scores
+        $('#player1-score').text(player1.score);
+        $('#player2-score').text(player2.score);
+
+        //Prints the cleared Current Roll and Turn Total on page
+        $('#roll').text(currentTurn.randNumber);
+        $('#roll-total').text(currentTurn.total);
+    });
 });
---
+++ js/scripts1.js
@@ -0,0 +1,85 @@
+function Player(playerName) {
+  this.playerName = playerName;
+  this.score = 0;
+};
+
+function takeTurn(player) {
+  this.sum = 0;
+  this.sumNumbers = 0;
+  this.player = player;
+
+};
+
+takeTurn.prototype.rollDice = function(playerOne, playerTwo) {
+  var sumNumbers = Math.floor(Math.random() * 6) + 1;
+  this.sum += sumNumbers;
+
+  if (sumNumbers == 1) {
+    this.sum = 0;
+    this.endTurn(playerOne, playerTwo);
+    return sumNumbers;
+  } else {
+    this.sumNumbers += sumNumbers;
+    return sumNumbers;
+  };
+};
+
+takeTurn.prototype.endTurn = function(playerOne, playerTwo) {
+  this.player.score += this.sum;
+  this.sum = 0;
+  this.sumNumbers = 0;
+  if (this.player == playerOne) {
+    this.player = playerTwo;
+    $("#playerTwo").toggleClass("active");
+    $("#playerOne").toggleClass("active");
+  } else if (this.player == playerTwo) {
+    this.player = playerOne;
+    $("#playerTwo").toggleClass("active");
+    $("#playerOne").toggleClass("active");
+
+  };
+
+};
+$(document).ready(function() {
+  var playerOne = new Player("Player One");
+  var playerTwo = new Player("Player Two");
+
+  var currentTurn = new takeTurn(playerOne);
+  var sum = currentTurn.sum;
+  $("#cumulScore").text(sum);
+  $("#playerOneScore").text(playerOne.score);
+  $("#playerTwoScore").text(playerTwo.score);
+  $("#current_player").text(currentTurn.player.playerName);
+  $("form#roll").submit(function(event) {
+    event.preventDefault();
+
+    var result = currentTurn.rollDice(playerOne, playerTwo);
+
+    $('#diceScore').text(result);
+    $('#cumulScore').text(currentTurn.sum);
+
+    if ((currentTurn.sum + currentTurn.player.score) >= 100) {
+      if (currentTurn.player == playerOne) {
+        $('#playerOneScore').text(currentTurn.sum + currentTurn.player.score);
+        alert("You are the winner!");
+      } else if (currentTurn.player == playerTwo) {
+        $('#playerTwoScore').text(currentTurn.sum + currentTurn.player.score)
+        alert("You are the winner!");
+      };
+    };
+  });
+
+  $("form#endTurn").submit(function(event) {
+    event.preventDefault();
+
+    currentTurn.endTurn(playerOne, playerTwo);
+
+    $('#current_player').text(currentTurn.player.playerName);
+
+    $('#playerOneScore').text(playerOne.score);
+    $('#playerTwoScore').text(playerTwo.score);
+
+    $('#roll').text(currentTurn.sumNumbers);
+    $('#cumulScore').text(currentTurn.sum);
+  });
+});
